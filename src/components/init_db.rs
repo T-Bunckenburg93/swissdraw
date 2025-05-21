be077e323ff16c3c1e7278aac8b6ae2343c64522
ruthless-nz <51357492+ruthless-nz@users.noreply.github.com>
2025-05-21 11:27:58 +0100

    init db if not exist

--- src/components/init_db.rs
+++ src/components/init_db.rs
@@ -10,15 +10,61 @@ use std::sync::Mutex;
 // #[component]
 pub fn Init_DB() -> Result<Connection> {
     let db_path = get_db_path();
-    Connection::open(db_path)
+    let conn = Connection::open(db_path)?;
+    init_db(&conn).expect("Failed to initialize DB");
+    Ok(conn)
 }
 
 // fn ChildComponent(cx: Scope) -> Element { let conn = use_context::<Rc<DbConnection>>(cx).expect("DB not provided");
 
 fn get_db_path() -> std::path::PathBuf {
-    let proj_dirs = ProjectDirs::from(".config", "swissdraw", "data").unwrap();
+    let proj_dirs = ProjectDirs::from(".config", "swissdraw", "data").unwrap(); // this is the path for irl
+    // let proj_dirs = ProjectDirs::from("com", "swissdraw", "swissdraw").unwrap();
     println!("Data dir: {:?}", proj_dirs.data_dir());
     let data_dir = proj_dirs.data_dir();
     std::fs::create_dir_all(data_dir).unwrap();
     data_dir.join("data.db")
+}
+
+
+// init the tables if they don't exist.
+fn init_db(conn: &Connection) -> Result<()> {
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS draw (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            last_modified DateTime DEFAULT CURRENT_TIMESTAMP
+        )",
+        [],
+    )?;
+// I want to lock the teams table so that its not updated.
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS teams (
+            sd_id INTEGER,
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            rank REAL
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS games (
+            sd_id INTEGER,
+            id INTEGER PRIMARY KEY,
+            team_a_id TEXT NOT NULL,
+            team_b_id TEXT NOT NULL,
+            team_a_score REAL,
+            team_b_score REAL,
+            played BOOLEAN DEFAULT FALSE,
+            streamed BOOLEAN DEFAULT FALSE,
+            _meta__is_current BOOLEAN DEFAULT TRUE,
+            _meta__is_deleted BOOLEAN DEFAULT FALSE,
+            _meta__last_modified DateTime DEFAULT CURRENT_TIMESTAMP
+        )",
+        [],
+    )?;
+    println!("DB tables created");
+    Ok(())
 }
