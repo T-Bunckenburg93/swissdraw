9b3359927363ebaa9d3790ad6bba5d7623878a4d
ruthless-nz <51357492+ruthless-nz@users.noreply.github.com>
2025-05-29 13:07:22 +0100

    rm junk route

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -14,7 +14,6 @@ pub fn Hero() -> Element {
             img { src: HEADER_SVG, id: "header", style: "width: 40%; height: auto;" }
             div { id: "links",
                 // The RSX macro also supports text nodes surrounded by quotes
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
                 Link { to: Route::New_Draw {}, "New Draw" }
                 Link { to: Route::Load_Draw {}, "Load Draw" }
                 Link { to: Route::Score_Prior_Games {}, "Score Results" }
